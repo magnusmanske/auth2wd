dbe036e901b9bea5dc43e27aff638664ca7d1b1e
Magnus Manske <mm6@sanger.ac.uk>
2023-01-25 09:02:14 +0000

    TESTING

--- src/merge_diff.rs
+++ src/merge_diff.rs
@@ -97,11 +97,11 @@ impl MergeDiff {
             .iter()
             .chain(self.altered_statements.values())
             .cloned()
-            .map(|c|{
+            /*.map(|c|{
                 let mut c = c;
                 c.set_references(vec![]); // TESTING remove references
                 c
-            })
+            }) */
             .map(|c|json!(c))
             .map(|c|{
                 let mut c = c;
