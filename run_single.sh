86bc35d8ccf9cf2eedc297e7c2c6ea1a5ab3296d
Magnus Manske <magnusmanske@googlemail.com>
2024-03-11 17:37:04 +0000

    misc

--- run_single.sh
+++ run_single.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-exho "NOT IMPLEMENTED YET"
-# toolforge jobs delete single
+toolforge jobs delete single
 
-# toolforge jobs run --wait --mem 2000Mi --cpu 1 --mount=all --image tool-listeria/tool-listeria:latest \
-# -o /data/project/listeria/listeria_rs/single.out -e /data/project/listeria/listeria_rs/single.err \
-# --command "sh -c 'target/release/main \"$1\" \"$2\" /data/project/listeria/listeria_rs/config.json'" single
+toolforge jobs run --wait --mem 2000Mi --cpu 1 --mount=all --image tool-ac2wd/tool-ac2wd:latest --command "sh -c 'target/release/auth2wd $1 $2'" single
 
-# toolforge jobs logs single
+toolforge jobs logs single
