e2f2591860026e4258592b5e526131197a67e45b
Magnus Manske <magnusmanske@googlemail.com>
2024-03-11 16:16:32 +0000

    misc

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+toolforge build start https://github.com/magnusmanske/auth2wd/
---
+++ restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+toolforge webservice buildservice stop
+toolforge webservice buildservice start --mount=none
+#toolforge jobs delete rustbot
+#toolforge jobs run --mem 5000Mi --cpu 3 --continuous --mount=all --image tool-listeria/tool-listeria:latest --command "sh -c 'target/release/bot /data/project/listeria/listeria_rs/config.json'" rustbot
