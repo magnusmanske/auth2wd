f313e679972ec5ed82ea6fbaac86a3379d35613d
Magnus Manske <magnusmanske@googlemail.com>
2025-09-10 16:34:12 +0100

    Fixed P3133 URL issue

--- src/external_importer.rs
+++ src/external_importer.rs
@@ -55,6 +55,7 @@ lazy_static! {
             (Regex::new(r"^https?://entities.oclc.org/worldcat/entity/([^.]+).html$").unwrap(),"${1}".to_string(),10832),
             (Regex::new(r"^https?://entities.oclc.org/worldcat/entity/([^.]+).jsonld$").unwrap(),"${1}".to_string(),10832),
             (Regex::new(r"^https?://www.filmportal.de/([A-Za-z0-9]+)$").unwrap(),"${1}".to_string(),2639),
+            (Regex::new(r"^https?://nektar.oszk.hu/resource/auth/([1-9]\d*)$").unwrap(),"${1}".to_string(),3133),
         ]
     };
 
